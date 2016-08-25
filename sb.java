9af20f2178e7fe3186852cb91ec5de96520a0c0e
shobanait <vshobanait2017@gmail.com>
2016-08-25 09:30:17 +0530

    Create sb.java

---
+++ sb.java
@@ -0,0 +1,1 @@
+import java.io.; import java.util.; public class Even { public static void main(String args[]) { Scanner sc=new Scanner(System.in); int num=sc.nextInt(); if(num%2==0) System.out.println("even"); else if(num%2==1) System.out.println("odd"); else if(num==0) System.out.println("odd"); } }
